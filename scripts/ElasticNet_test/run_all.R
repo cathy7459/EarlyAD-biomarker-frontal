03e5da36529d1830d95e801a8291a8b8b61bf9ce
cathy7459 <cathy7459@ewhain.net>
2026-03-12 17:07:08 +0900

    Delete scripts/ElasticNet_test directory

--- scripts/ElasticNet_test/run_all.R
+++
@@ -1,6 +0,0 @@
-# run_all.R
-source("scripts/ElasticNet_test/00_setup.R")
-source("scripts/ElasticNet_test/01_core_genes.R")
-source("scripts/ElasticNet_test/02_test_no_vasAD.R")
-source("scripts/ElasticNet_test/03_test.R")
-source("scripts/ElasticNet_test/04_fit.R")
