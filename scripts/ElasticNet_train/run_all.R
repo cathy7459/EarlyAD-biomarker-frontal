3eee989791961a1ce077cb396d2720c998e2f1b0
cathy7459 <cathy7459@ewhain.net>
2026-03-12 17:06:56 +0900

    Delete scripts/ElasticNet_train directory

--- scripts/ElasticNet_train/run_all.R
+++
@@ -1,4 +0,0 @@
-# Run the full pipeline (paper-style)
-source("scripts/ElasticNet_train/01_proj_expression_gse33000.R")
-source("scripts/ElasticNet_train/02_deg_effect_filter.R")
-source("scripts/ElasticNet_train/03_lasso_train.R")
